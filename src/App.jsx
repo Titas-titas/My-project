c27183a318ca42cc071a2d68e1be3b64ea05e5ac
Titas-titas <titas.kucinskas@gmail.com>
2024-12-11 16:03:33 +0200

    added style selected icon

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import TVSeries from "./components/TVSeries";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import { useState } from "react";
 
 function App() {
   const location = useLocation();
   const isLoginPage = location.pathname === "/login" || location.pathname === "/signup";
+  const [selectedIcon, setSelectedIcon] = useState(location.pathname);
+
+  const handleIconClick = (path) => {
+    setSelectedIcon(path);
+  };
 
   return (
     <>
@@ -19,10 +25,34 @@ function App() {
               <img src="/logo.svg" alt="Logo" />
             </div>
             <div className="grupe">
-              <Link to="/" className="nav-item"><img src="/icon-nav-home.svg" alt="Home" /></Link>
-              <Link to="/movies" className="nav-item"><img src="/icon-nav-movies.svg" alt="Movies" /></Link>
-              <Link to="/series" className="nav-item"><img src="/icon-nav-tv-series.svg" alt="TV Series" /></Link>
-              <Link to="/bookmarks" className="nav-item"><img src="/icon-nav-bookmark.svg" alt="Bookmarks" /></Link>
+              <Link
+                to="/"
+                className={`nav-item ${selectedIcon === "/" ? "selected" : ""}`}
+                onClick={() => handleIconClick("/")}
+              >
+                <img src="/icon-nav-home.svg" alt="Home" />
+              </Link>
+              <Link
+                to="/movies"
+                className={`nav-item ${selectedIcon === "/movies" ? "selected" : ""}`}
+                onClick={() => handleIconClick("/movies")}
+              >
+                <img src="/icon-nav-movies.svg" alt="Movies" />
+              </Link>
+              <Link
+                to="/series"
+                className={`nav-item ${selectedIcon === "/series" ? "selected" : ""}`}
+                onClick={() => handleIconClick("/series")}
+              >
+                <img src="/icon-nav-tv-series.svg" alt="TV Series" />
+              </Link>
+              <Link
+                to="/bookmarks"
+                className={`nav-item ${selectedIcon === "/bookmarks" ? "selected" : ""}`}
+                onClick={() => handleIconClick("/bookmarks")}
+              >
+                <img src="/icon-nav-bookmark.svg" alt="Bookmarks" />
+              </Link>
             </div>
             <div className="profile-container">
               <Link to="/login">
@@ -48,4 +78,3 @@ function App() {
 }
 
 export default App;
-
