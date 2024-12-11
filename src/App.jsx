82db8a3e0e21d586b4ba4c120a8452ac69e4273c
Titas-titas <titas.kucinskas@gmail.com>
2024-12-11 10:11:43 +0200

    added SignUp code / fixed index.css style

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,32 @@ import Movie from "./components/Movies";
 import TVSeries from "./components/TVSeries";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   const location = useLocation();
-  const isLoginPage = location.pathname === "/login";
+  const isLoginPage = location.pathname === "/login" || location.pathname === "/signup";
 
   return (
     <>
       <div className={!isLoginPage ? "header" : ""}>
         {!isLoginPage && (
-          
-            <div className="Bar">
-              <div className="logo">
-                <img src="/logo.svg" alt="Logo" />
-              </div>
-              <div className="grupe">
-                <Link to="/" className="nav-item"><img src="/icon-nav-home.svg" alt="Home" /></Link>
-                <Link to="/movies" className="nav-item"><img src="/icon-nav-movies.svg" alt="Movies" /></Link>
-                <Link to="/series" className="nav-item"><img src="/icon-nav-tv-series.svg" alt="TV Series" /></Link>
-                <Link to="/bookmarks" className="nav-item"><img src="/icon-nav-bookmark.svg" alt="Bookmarks" /></Link>
-              </div>
-              <div className="profile-container">
-                <Link to="/login">
-                  <img src="/image-avatar.png" alt="Profile" className="profile-picture" />
-                </Link>
-              </div>
+          <div className="Bar">
+            <div className="logo">
+              <img src="/logo.svg" alt="Logo" />
             </div>
-          
+            <div className="grupe">
+              <Link to="/" className="nav-item"><img src="/icon-nav-home.svg" alt="Home" /></Link>
+              <Link to="/movies" className="nav-item"><img src="/icon-nav-movies.svg" alt="Movies" /></Link>
+              <Link to="/series" className="nav-item"><img src="/icon-nav-tv-series.svg" alt="TV Series" /></Link>
+              <Link to="/bookmarks" className="nav-item"><img src="/icon-nav-bookmark.svg" alt="Bookmarks" /></Link>
+            </div>
+            <div className="profile-container">
+              <Link to="/login">
+                <img src="/image-avatar.png" alt="Profile" className="profile-picture" />
+              </Link>
+            </div>
+          </div>
         )}
 
         <div className={!isLoginPage ? "films" : ""}>
@@ -40,6 +39,7 @@ function App() {
             <Route path="/series" element={<TVSeries />} />
             <Route path="/bookmarks" element={<Bookmarks />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </div>
       </div>
@@ -48,3 +48,4 @@ function App() {
 }
 
 export default App;
+
